a9dd33389e126c66fadfb90500e0938c2bff5541
Alex Gavrilescu <leskcorp@gmail.com>
2025-07-12 19:00:53 +0200

    Bugfix: allow scrolling on the task list page

--- src/web/components/Layout.tsx
+++ src/web/components/Layout.tsx
@@ -38,7 +38,7 @@ export default function Layout({
 			/>
 			<div className="flex-1 flex flex-col min-h-0">
 				<Navigation projectName={projectName} />
-				<main className="flex-1 overflow-hidden">
+				<main className="flex-1 overflow-auto">
 					<Outlet context={{ tasks, docs, decisions, isLoading, onRefreshData }} />
 				</main>
 			</div>
